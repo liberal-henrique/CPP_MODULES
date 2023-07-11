ebd21269916c400bef166c43bb9758453d2cd1ae
Luiz henrique Liberal de souza <lliberal@c2r8s1.42lisboa.com>
2023-07-11 13:42:16 +0100

    finished

---
+++ CPP_Module_00/ex01/Contact.cpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lliberal <lliberal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/28 10:41:17 by lliberal          #+#    #+#             */
+/*   Updated: 2023/07/10 16:42:25 by lliberal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+
+Contact::Contact() {
+	first_name = "";
+	last_name = "";
+	nickname = "";
+	phone_number = "";
+	darkest_secret = "";
+}
+
+Contact::Contact(std::string first, std::string last, std::string nick, int phone, std::string secret)
+:first_name(first)
+ {
+	last_name = last;
+	nickname = nick;
+	phone_number = phone;
+	darkest_secret = secret;
+}
+
+Contact::~Contact() {
+	first_name.clear();
+	last_name.clear();
+	nickname.clear();
+	phone_number.clear();
+	darkest_secret.clear();
+}
+
+std::string	Contact::GetFirst_Name() const {
+	return (first_name);
+}
+
+std::string	Contact::GetLast_Name() const {
+	return (last_name);
+}
+
+std::string	Contact::GetNickname() const {
+	return (nickname);
+}
+
+std::string	Contact::GetPhone_number() const {
+	return (phone_number);
+}
+
+std::string	Contact::GetDarkest_Secret() const {
+	return (darkest_secret);
+}
+
+void	Contact::SetFirst_Name(std::string new_first_name) {
+	this->first_name = new_first_name;
+}
+
+void	Contact::SetLast_Name(std::string new_last_name) {
+	this->last_name = new_last_name;
+}
+
+void	Contact::SetNickname(std::string new_nickname) {
+	this->nickname = new_nickname;
+}
+
+void	Contact::SetPhone_Number(std::string new_phone_number) {
+	this->phone_number = new_phone_number;
+}
+
+void	Contact::SetDarkest_Secret(std::string new_darkest_secret) {
+	this->darkest_secret = new_darkest_secret;
+}
+
---
+++ CPP_Module_00/ex01/Contact.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lliberal <lliberal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/28 10:40:21 by lliberal          #+#    #+#             */
+/*   Updated: 2023/06/28 15:18:03 by lliberal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <ctype.h>
+#include <string>
+#include <iostream>
+#include <cstring>
+
+class Contact
+{
+	private:
+		std::string first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string phone_number;
+		std::string darkest_secret;
+
+	public:
+		Contact();
+		~Contact();
+		Contact(std::string first, std::string last, std::string nick, int phone, std::string secret);
+
+		void	SetFirst_Name(std::string new_first_name);
+		void	SetLast_Name(std::string new_last_name);
+		void	SetNickname(std::string new_nickname);
+		void	SetPhone_Number(std::string new_phone_number);
+		void	SetDarkest_Secret(std::string new_darkest_secret);
+
+		std::string	GetFirst_Name() const;
+		std::string	GetLast_Name() const;
+		std::string	GetNickname() const;
+		std::string	GetPhone_number() const;
+		std::string	GetDarkest_Secret() const;
+};
+
+#endif
